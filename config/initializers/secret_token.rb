44fcbde653d52229882d9dff7068b2f427f86d35
elite.ljp <elite.ljp@gmail.com>
2010-07-29 21:41:39 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'b8dd285ae226edf464d3b3d3e53ec22d3a9be74e71b8856ce2f0636930357435997c4386584d96a8ef5977371bfdc18052ca62459005bb37783e0fe9a005b7ec'
